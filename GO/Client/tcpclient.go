d31a1260941baa4d8900bfad105cbcd2ef7db3fc
yanjunGE <98467248+yanjunGE@users.noreply.github.com>
2023-02-06 09:58:37 +0100

    Update tcpclient.go
    final version

--- GO/Client/tcpclient.go
+++ GO/Client/tcpclient.go
@@ -5,69 +5,67 @@ import (
 	"io"
 	"net"
 	"os"
+	"strconv"
 )
 
-// 发送文件到服务端
-func SendFile(filePath string, fileSize int64, conn net.Conn) {
+// send file to serveur
+func SendFile(filePath string, conn net.Conn) {
 	f, err := os.Open(filePath)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer f.Close()
-	var count int64
+
 	for {
 		buf := make([]byte, 2048)
-		//读取文件内容
+		//read the file
 		n, err := f.Read(buf)
-		if err != nil && io.EOF == err {
-			fmt.Println("文件传输完成")
-			//告诉服务端结束文件接收
-			//conn.Write([]byte("ok"))
-			return
+		if err != nil && err == io.EOF {
+			fmt.Println("transform end")
+			break
 		}
-		//发送给服务端
+		//send the file to serveur
 		conn.Write(buf[:n])
-
-		count += int64(n)
-		sendPercent := float64(count) / float64(fileSize) * 100
-		value := fmt.Sprintf("%.2f", sendPercent)
-		//打印上传进度
-		fmt.Println("文件上传：" + value + "%")
+		fmt.Println("uploading......")
 	}
+	//conn.Write([]byte("ok"))
 }
-func Handler(conn net.Conn) {
+func Handler(conn net.Conn, fileName string) {
 	buf := make([]byte, 2048)
-	//读取客户端发送的内容
+	//read the information from serveur
+	resFileName := "res" + fileName
 	n, err := conn.Read(buf)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	resFileName := string(buf[:n])
-
-	//获取客户端ip+port
-	addr := conn.RemoteAddr().String()
-	fmt.Println(addr + ": 文件被存储为为--" + resFileName)
-	//告诉客户端已经接收到文件名
+	resfileSizeStr := string(buf[:n])
+	resfileSize, _ := strconv.Atoi(resfileSizeStr)
+	//get the address and port
+	//addr := conn.RemoteAddr().String()
+	fmt.Println(" file is stocked as--" + resFileName)
+	//tell the server the connection is ok
 	conn.Write([]byte("ok"))
-	//创建文件
+	//creat file
 	f, err := os.Create(resFileName)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	//循环接收客户端传递的文件内容
-	buf = make([]byte, 2048)
+	//receive the file
+	buf = make([]byte, 4096)
+	received := 0
 	for {
-		fmt.Print(buf[:n])
-		fmt.Print("1")
 		n, err4 := conn.Read(buf)
+		//fmt.Print(buf[:n])
+		fmt.Print(err4)
+
 		if err4 != nil {
 			if err4 == io.EOF {
-				fmt.Println("文件接受完毕")
+				fmt.Println("download end")
 				//i = NO_ROUTE
-				fmt.Println(addr + ": 协程结束")
+				fmt.Println(": process end")
 				//runtime.Goexit()
 				break
 
@@ -82,41 +80,53 @@ func Handler(conn net.Conn) {
 			fmt.Println(addr + ": 协程结束")
 			runtime.Goexit()
 		}*/
+		//revData := string(buf[:n])
+		//print(revData + "!!!")
+		/*if revData == "ok" {
+			//send the file
+			print("ok")
+			break
+		}*/
+		received += len(buf[:n])
 		f.Write(buf[:n])
+		if received >= resfileSize {
+			break
+			//print(received)
+		}
+
 	}
 	//defer conn.Close()
 	fmt.Println("fclose")
-	defer f.Close()
+	f.Close()
 	fmt.Println("close")
-
+	conn.Close()
 }
 
 func main() {
-	fmt.Print("请输入文件的完整路径：")
-	//创建切片，用于存储输入的路径
+	fmt.Print("please type in the path of file：")
+	//store the path in str
 	var str string
 	fmt.Scan(&str)
-	//获取文件信息
+	//get the information necessary
 	fileInfo, err := os.Stat(str)
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	//创建客户端连接
+	//creat a connection with server
 	conn, err := net.Dial("tcp", ":8000")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	defer conn.Close()
-	//文件名称
+	//name of the file
 	fileName := fileInfo.Name()
-	//文件大小
 	fileSize := fileInfo.Size()
-	//发送文件名称到服务端
+	//send the name of file to serveur
 	conn.Write([]byte(fileName))
 	buf := make([]byte, 2048)
-	//读取服务端内容
+	//read the answer of serveur
 	n, err := conn.Read(buf)
 	if err != nil {
 		fmt.Println(err)
@@ -124,25 +134,32 @@ func main() {
 	}
 	revData := string(buf[:n])
 	if revData == "ok" {
-		//发送文件数据
-		SendFile(str, fileSize, conn)
+		fileSizeStr := strconv.FormatInt(fileSize, 10)
+		conn.Write([]byte(fileSizeStr))
+		SendFile(str, conn)
 	}
-	listen, err := net.Listen("tcp", ":8001")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer listen.Close()
-	for {
-		//阻塞等待客户端
-		conn, err := listen.Accept()
+
+	//----------new connect to recieve---------
+	/*	listen, err := net.Listen("tcp", ":8001")
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		//创建协程
-		defer conn.Close()
-		Handler(conn)
+		defer listen.Close()
+		for {
+			//wait for conection
+			conn, err := listen.Accept()
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			//handle the connection
+			defer conn.Close()
+			go Handler(conn)
 
-	}
+		}
+	*/
+
+	Handler(conn, fileName)
+
 }
